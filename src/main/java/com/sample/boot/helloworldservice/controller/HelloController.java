c0a3634eff6f06ea8cc7632027934e0d5df0cf9b
rishabkashyap <rishabkashyap16@gmail.com>
2026-05-07 18:19:01 +0530

    changes

--- src/main/java/com/sample/boot/helloworldservice/controller/HelloController.java
+++ src/main/java/com/sample/boot/helloworldservice/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.sample.boot.helloworldservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1")
+@RestController
+@RequestMapping("/v1")
 public class HelloController {
     @GetMapping("/test")
     public String hello() {
